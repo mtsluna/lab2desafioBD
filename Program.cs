d02133606dd38ffc1a2b90f98d3049ec58f5b0ea
mtsluna <mtsluna@gmail.com>
2020-10-08 16:24:24 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace db
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            MySqlConnection mySql = new MySqlConnection("Server=localhost; Database=ejemplo; Uid=root; Pwd=");
+
+            try
+            {
+                mySql.Open();
+                Console.WriteLine("Se abrio correctamete");
+
+                MySqlCommand mySqlCommand = new MySqlCommand("SELECT * FROM personas", mySql);
+                MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter();
+                mySqlDataAdapter.SelectCommand = mySqlCommand;
+                DataTable dataTable = new DataTable();
+                mySqlDataAdapter.Fill(dataTable);
+                foreach(DataRow dataRow in dataTable.Rows)
+                {
+                    Console.WriteLine(dataRow["dni"]+" - "+ dataRow["nombre"]+" - "+ dataRow["apellido"]);
+                }
+
+            } catch(Exception e)
+            {
+                Console.WriteLine("No se abrio correctamete");
+            }
+
+            Console.WriteLine("Para salir presione cualquier tecla...");
+            Console.ReadKey();
+
+        }
+    }
+}
